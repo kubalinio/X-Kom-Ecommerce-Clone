ae34a69d5176e7f3b85559a98149eb5c7a54449c
Jakub <jsapalawebdev@gmail.com>
2023-07-14 17:56:42 +0200

    Created components folder in features fav list

--- src/features/favList/index.tsx
+++ src/features/favList/index.tsx
@@ -1,6 +1,6 @@
 export { AddToFavModal } from './AddToFavModal'
 export { AddToFavPopper } from './AddToFavPopper'
+export { DeleteListBtn } from './components/ui/DeleteListBtn'
 export { useDeleteFavList } from './dataAccess/mutations/deleteFavList'
 export { FavListBtn } from './FavListBtn'
-export { DeleteListBtn } from './ui/DeleteListBtn'
 export { useFavList } from './useFavList'
