0321f158075b7c4c49fe9154e1cd8c498bcf7728
Jakub <jsapalawebdev@gmail.com>
2023-07-16 18:25:36 +0200

    fix tsconfig issue

--- src/app/(shop)/(favlists)/layout.tsx
+++ src/app/(shop)/(favlists)/layout.tsx
@@ -1,10 +1,10 @@
-import { NavUser } from '@/features/favlistPage/listsPage/containers/navUser'
+import { NavUser } from '@/features/favlistPage/listsPage/containers/NavUser/NavUserContainer'
 import { NeedHelpInfo } from '@/features/favlistPage/listsPage/containers/userShopList/NeedHelpInfo'
 
 export default function ListsLayout({ children }: { children: React.ReactNode }) {
   return (
     <main className="mx-auto mt-5 w-[calc(100%-32px)] md:w-[calc(100%-48px)] lg:mt-6 lg:w-[calc(100%-64px)] lg:max-w-[1156px] 2xl:max-w-[1444px]">
-      <div className="w-full pb-8 pt-1 md:pb-10 md:pt-3 lg:pt-5">
+      <div className="w-full pt-1 pb-8 md:pb-10 md:pt-3 lg:pt-5">
         {/* Flex Container */}
         <div className="flex flex-wrap">
           {/* Left Section only Desktop*/}
--- src/features/favlistPage/listsPage/containers/NavUser/NavUserBody.tsx
+++ src/features/favlistPage/listsPage/containers/NavUser/NavUserBody.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from 'react'
 import { navUserItems } from '@/app/(shop)/(favlists)/listy/page'
 
 const IconNavUser = ({ icon }: { icon: ReactNode }) => (
-  <span className="inline-block h-6 w-6 text-2xl md:mr-3">{icon}</span>
+  <span className="inline-block w-6 h-6 text-2xl md:mr-3">{icon}</span>
 )
 
 type UserBody = {
@@ -20,9 +20,8 @@ export const NavUserBody = ({ activeLink, links }: UserBody) => {
           key={link.slug + Math.random()}
           href={link.slug}
           title={link.title}
-          className={`${
-            activeLink?.includes(link.slug) ? 'font-bold' : 'font-normal'
-          } flex items-center px-4 py-3 hover:bg-gray-100`}
+          className={`${activeLink?.includes(link.slug) ? 'font-bold' : 'font-normal'
+            } flex items-center px-4 py-3 hover:bg-gray-100`}
         >
           <IconNavUser icon={activeLink?.includes(link.slug) ? link.iconActive : link.icon} />
 
